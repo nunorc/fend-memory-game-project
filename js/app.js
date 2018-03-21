4d2f1c9201ce2d9260943dcff130ec2237c22eb9
Nuno Carvalho <mestre.smash@gmail.com>
2018-03-21 11:24:38 +0000

    Game logic done

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
-/*
- * Create a list that holds all of your cards
- */
 
+// define some constants
+const STARS_THRESHOLDS = [30, 20, 10];  // number of moves for loosing stars
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// define some global variables for the stopwatch
+let seconds = 0, minutes = 0, hours = 0, t;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,14 +20,160 @@ function shuffle(array) {
     return array;
 }
 
+// function to handle a card click
+function cardClicked(event) {
+    const card = event.target;
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+    card.classList.toggle('open');
+    card.classList.toggle('show');
+
+    cardsVerifyMatch();
+}
+
+// functin that verify if there is a match
+function cardsVerifyMatch() {
+    const allOpenCards = document.querySelectorAll('.open');
+    const openCards = [];
+    for (const card of allOpenCards) {
+        if (!(card.classList.contains('match'))) {
+            openCards.push(card);
+        }
+    }
+
+    // when two cards open verify if they are a match
+    if (openCards.length == 2) {
+        // increment moves
+        incMoves();
+
+        // check cards
+        const i1 = openCards[0].firstElementChild.className;
+        const i2 = openCards[1].firstElementChild.className;
+        if (i1 === i2) {
+            for (const card of openCards) {
+                card.classList.toggle('match');
+            }
+
+            // after a match found verify if game ends
+            gameEnds();
+        }
+        else {
+            // if open cards do not match close them
+            cardsClose(openCards);
+        }
+    }
+}
+
+// increment moves counts
+function incMoves() {
+    // update moves counter
+    let moves = document.querySelector('.moves').textContent;
+    moves = Number(moves) + 1;
+    document.querySelector('.moves').textContent = moves;
+
+    // update moves text (singular or plural)
+    let movesText = 'Moves';
+    if (moves == 1) {
+        movesText = 'Move';
+    }
+    document.querySelector('.moves-text').textContent = movesText;
+
+    // update stars
+    const stars = document.querySelectorAll('.star');
+    for (let i = 0; i < STARS_THRESHOLDS.length; i++) {
+        if (moves > STARS_THRESHOLDS[i]) {
+            stars[i].firstElementChild.className = 'fa fa-star-o';
+        }
+    }
+
+}
+
+// close a list of open cards given as argument
+function cardsClose(cards) {
+    setTimeout( function() {
+        for (const card of cards) {
+            card.classList.remove('show', 'open');
+        }
+    }, 1000);
+}
+
+// verify if game ended, i.e. all cards are a match
+function gameEnds() {
+    const allMatchCards = document.querySelectorAll('.match');
+    if (allMatchCards.length == 16) {
+        const moves = document.querySelector('.moves').textContent;
+        const time = document.querySelector('.stopwatch').textContent;
+        alert('GAME WIN in '+time+' and '+moves+' moves');
+        clearTimeout(t);
+        restartGame();
+    }
+}
+
+// start/restart the game
+function restartGame() {
+    // shuffe board
+    const myDeck = document.querySelector('.deck');
+    const shuffled = shuffle([...document.querySelectorAll('.card')]);
+    myDeck.innerHTML = '';
+    for (const i of shuffled) {
+        myDeck.appendChild(i);
+    }
+
+    // close all cards
+    const cards = document.querySelectorAll('.card');
+    for (const card of cards) {
+        card.classList.remove('open', 'match', 'show');
+    }
+
+    // reset moves counter
+    document.querySelector('.moves').textContent = '0';
+    document.querySelector('.moves-text').textContent = 'Moves';
+
+    // reset stars
+    const stars = document.querySelectorAll('.star');
+    for (star of stars) {
+        star.firstElementChild.className = 'fa fa-star';
+    }
+
+    // reset stopwatch
+    clearTimeout(t);
+    seconds = 0; minutes = 0; hours = 0;
+    timer();
+}
+
+// handle stopwatch
+function tick() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+
+    document.querySelector('.stopwatch').textContent =
+      (hours ? (hours > 9 ? hours : "0" + hours) : "00")
+        + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00")
+          + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+
+// function to start stopwatch
+function timer() {
+    t = setTimeout(tick, 1000);
+}
+
+// add click event for all cards
+const cards = document.querySelectorAll('.card');
+for (const card of cards) {
+  card.addEventListener('click', cardClicked);
+}
+
+// add click event for restart button
+document.querySelector('.restart').addEventListener('click', restartGame);
+
+// START GAME -- begin
+restartGame();
+
